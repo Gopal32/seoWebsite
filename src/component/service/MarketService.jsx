d6e341a53ca1b9d97a7019f96cab57df2f51d12e
Gopal32 <gkothari96@gmail.com>
2023-02-04 10:36:57 +0530

    Changed class in className on marketservice file

--- src/component/service/MarketService.jsx
+++ src/component/service/MarketService.jsx
@@ -7,7 +7,7 @@ function MarketService() {
         <div className="div-service-slogan">
           <div className="container-fluid">
             <div className="row my-5">
-              <div className="col-md-5 ornageVerticalborder" style={{ backgroundColor: "white" }}>
+              <div className="col-md-5 ornageVerticalborder">
                 <div className="text-right">
                   <h1 className="mx-4">
                     Best Digital
@@ -17,9 +17,11 @@ function MarketService() {
                 </div>
               </div>
               <div className="col-md-6 mx-4">
-                <p style={{ lineHeight : "30px", fontSize: "18px"}}>Start working with an company that can provide everything you need to generate 
-                  awareness, drive traffic, connect with customers, and increase sales montes,
-                  nascetur ridiculus mus.</p>
+                <p style={{ lineHeight: "30px", fontSize: "18px" }}>
+                  Start working with an company that can provide everything you
+                  need to generate awareness, drive traffic, connect with
+                  customers, and increase sales montes, nascetur ridiculus mus.
+                </p>
               </div>
               <div
                 className="col-md-7"
@@ -28,22 +30,55 @@ function MarketService() {
             </div>
             <div className="row my-5">
               <div className="col-md card m-3">
-                <div className="text-center my-4"><img src="assests/images/feature1.png" alt="" /></div>
-                <div className="text-center"><h2>Socail Marketing</h2></div>
-                <div className="text-center mt-2 px-3">Start working with an company that provide everything you need to any create awareness drive</div>
-                <div className="readMorebutton mt-2 mb-4 text-center"><a href="#top"><b>Read More</b></a></div>
+                <div className="text-center my-4">
+                  <img src="assests/images/feature1.png" alt="" />
+                </div>
+                <div className="text-center">
+                  <h2>Socail Marketing</h2>
+                </div>
+                <div className="text-center mt-2 px-3">
+                  Start working with an company that provide everything you need
+                  to any create awareness drive
+                </div>
+                <div className="readMorebutton mt-2 mb-4 text-center">
+                  <a href="#top">
+                    <b>Read More</b>
+                  </a>
+                </div>
               </div>
               <div className="col-md card m-3">
-                <div className="text-center my-4"><img src="assests/images/feature2.png" alt="" /></div>
-                <div className="text-center"><h2>SEO Optimization</h2></div>
-                <div className="text-center mt-2 px-3">Start working with an company that provide everything you need to any create awareness drive</div>
-                <div className="readMorebutton mt-2 mb-4 text-center"><a href="#top"><b>Read More</b></a></div>
+                <div className="text-center my-4">
+                  <img src="assests/images/feature2.png" alt="" />
+                </div>
+                <div className="text-center">
+                  <h2>SEO Optimization</h2>
+                </div>
+                <div className="text-center mt-2 px-3">
+                  Start working with an company that provide everything you need
+                  to any create awareness drive
+                </div>
+                <div className="readMorebutton mt-2 mb-4 text-center">
+                  <a href="#top">
+                    <b>Read More</b>
+                  </a>
+                </div>
               </div>
               <div className="col-md card m-3">
-                <div className="text-center my-4"><img src="assests/images/feature3.png" alt="" /></div>
-                <div className="text-center"><h2>SEO Management</h2></div>
-                <div className="text-center mt-2 px-3">Start working with an company that provide everything you need to any create awareness drive</div>
-                <div className="readMorebutton mt-2 mb-4 text-center"><a href="#top"><b>Read More</b></a></div>
+                <div className="text-center my-4">
+                  <img src="assests/images/feature3.png" alt="" />
+                </div>
+                <div className="text-center">
+                  <h2>SEO Management</h2>
+                </div>
+                <div className="text-center mt-2 px-3">
+                  Start working with an company that provide everything you need
+                  to any create awareness drive
+                </div>
+                <div className="readMorebutton mt-2 mb-4 text-center">
+                  <a href="#top">
+                    <b>Read More</b>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
@@ -62,80 +97,155 @@ function MarketService() {
                 <div className="orangeBorder"></div>
 
                 {/* <div className="mx-5"> */}
-                  <p style={{ lineHeight : "30px", fontSize: "18px"}}>
-                    Start working with an company that can provide everything you 
-                    need to generate awareness, drive traffic, connect with customers, 
-                    and increase sales nascetur ridiculus mus. Donec quam felis, 
-                    ultricies nec, pellentesque eu.
-                  </p>
-                  <p style={{ lineHeight : "30px", fontSize: "18px"}}>Donec quam felis, ultricies nec,
-                    pellentesque eu, pretium quis, sem. 
-                    Nulla consequat massa quis enim</p>
+                <p style={{ lineHeight: "30px", fontSize: "18px" }}>
+                  Start working with an company that can provide everything you
+                  need to generate awareness, drive traffic, connect with
+                  customers, and increase sales nascetur ridiculus mus. Donec
+                  quam felis, ultricies nec, pellentesque eu.
+                </p>
+                <p style={{ lineHeight: "30px", fontSize: "18px" }}>
+                  Donec quam felis, ultricies nec, pellentesque eu, pretium
+                  quis, sem. Nulla consequat massa quis enim
+                </p>
                 {/* </div> */}
 
                 <div className="col text-center">
-                  <button type="button" class="btn btn-warning btnWidth bgorange">LEARN MORE</button>
-                  <button type="button" class="btn btn-warning btnWidth">CONTACT US</button>
+                  <button
+                    type="button"
+                    className="btn btn-warning btnWidth bgorange"
+                  >
+                    LEARN MORE
+                  </button>
+                  <button type="button" className="btn btn-warning btnWidth">
+                    CONTACT US
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-     
 
       <div className="div-market-service red">
         <div className="div-service-slogan">
           <div className="container-fluid">
             <div className="text-center">
               <h6>OUR SERVICES</h6>
-              <h1><span>Our Best</span><span className="orangeColor"> Services</span></h1>
+              <h1>
+                <span>Our Best</span>
+                <span className="orangeColor"> Services</span>
+              </h1>
             </div>
             <div className="orangeBorder"></div>
             <div className="row my-5">
               <div className="col-md m-3 services">
-                <div className="text-center my-4"><img src="assests/images/service1.png" alt="" /></div>
-                <div className="text-center"><h2>Socail Marketing</h2></div>
-                <div className="text-center mt-2 px-4">One of the top 100 advertising and marketing agencies knows what it means to be</div>
-                <div className="readMorebutton mt-2 mb-4 text-center"><a href="#top"><b>Read More</b></a></div>
+                <div className="text-center my-4">
+                  <img src="assests/images/service1.png" alt="" />
+                </div>
+                <div className="text-center">
+                  <h2>Socail Marketing</h2>
+                </div>
+                <div className="text-center mt-2 px-4">
+                  One of the top 100 advertising and marketing agencies knows
+                  what it means to be
+                </div>
+                <div className="readMorebutton mt-2 mb-4 text-center">
+                  <a href="#top">
+                    <b>Read More</b>
+                  </a>
+                </div>
               </div>
               <div className="col-md m-3 services">
-                <div className="text-center my-4"><img src="assests/images/service2.png" alt="" /></div>
-                <div className="text-center"><h2>SEO Optimization</h2></div>
-                <div className="text-center mt-2 px-4">One of the top 100 advertising and marketing agencies knows what it means to be</div>
-                <div className="readMorebutton mt-2 mb-4 text-center"><a href="#top"><b>Read More</b></a></div>
+                <div className="text-center my-4">
+                  <img src="assests/images/service2.png" alt="" />
+                </div>
+                <div className="text-center">
+                  <h2>SEO Optimization</h2>
+                </div>
+                <div className="text-center mt-2 px-4">
+                  One of the top 100 advertising and marketing agencies knows
+                  what it means to be
+                </div>
+                <div className="readMorebutton mt-2 mb-4 text-center">
+                  <a href="#top">
+                    <b>Read More</b>
+                  </a>
+                </div>
               </div>
               <div className="col-md m-3 services">
-                <div className="text-center my-4"><img src="assests/images/service3.png" alt="" /></div>
-                <div className="text-center"><h2>SEO Management</h2></div>
-                <div className="text-center mt-2 px-4">One of the top 100 advertising and marketing agencies knows what it means to be</div>
-                <div className="readMorebutton mt-2 mb-4 text-center"><a href="#top"><b>Read More</b></a></div>
+                <div className="text-center my-4">
+                  <img src="assests/images/service3.png" alt="" />
+                </div>
+                <div className="text-center">
+                  <h2>SEO Management</h2>
+                </div>
+                <div className="text-center mt-2 px-4">
+                  One of the top 100 advertising and marketing agencies knows
+                  what it means to be
+                </div>
+                <div className="readMorebutton mt-2 mb-4 text-center">
+                  <a href="#top">
+                    <b>Read More</b>
+                  </a>
+                </div>
               </div>
             </div>
             <div className="row my-5">
               <div className="col-md m-3 services">
-                <div className="text-center my-4"><img src="assests/images/service4.png" alt="" /></div>
-                <div className="text-center"><h2>Socail Marketing</h2></div>
-                <div className="text-center mt-2 px-4">One of the top 100 advertising and marketing agencies knows what it means to be</div>
-                <div className="readMorebutton mt-2 mb-4 text-center"><a href="#top"><b>Read More</b></a></div>
+                <div className="text-center my-4">
+                  <img src="assests/images/service4.png" alt="" />
+                </div>
+                <div className="text-center">
+                  <h2>Socail Marketing</h2>
+                </div>
+                <div className="text-center mt-2 px-4">
+                  One of the top 100 advertising and marketing agencies knows
+                  what it means to be
+                </div>
+                <div className="readMorebutton mt-2 mb-4 text-center">
+                  <a href="#top">
+                    <b>Read More</b>
+                  </a>
+                </div>
               </div>
               <div className="col-md m-3 services">
-                <div className="text-center my-4"><img src="assests/images/service5.png" alt="" /></div>
-                <div className="text-center"><h2>SEO Optimization</h2></div>
-                <div className="text-center mt-2 px-4">One of the top 100 advertising and marketing agencies knows what it means to be</div>
-                <div className="readMorebutton mt-2 mb-4 text-center"><a href="#top"><b>Read More</b></a></div>
+                <div className="text-center my-4">
+                  <img src="assests/images/service5.png" alt="" />
+                </div>
+                <div className="text-center">
+                  <h2>SEO Optimization</h2>
+                </div>
+                <div className="text-center mt-2 px-4">
+                  One of the top 100 advertising and marketing agencies knows
+                  what it means to be
+                </div>
+                <div className="readMorebutton mt-2 mb-4 text-center">
+                  <a href="#top">
+                    <b>Read More</b>
+                  </a>
+                </div>
               </div>
               <div className="col-md m-3 services">
-                <div className="text-center my-4"><img src="assests/images/service6.png" alt="" /></div>
-                <div className="text-center"><h2>SEO Management</h2></div>
-                <div className="text-center mt-2 px-4">One of the top 100 advertising and marketing agencies knows what it means to be</div>
-                <div className="readMorebutton mt-2 mb-4 text-center"><a href="#top"><b>Read More</b></a></div>
+                <div className="text-center my-4">
+                  <img src="assests/images/service6.png" alt="" />
+                </div>
+                <div className="text-center">
+                  <h2>SEO Management</h2>
+                </div>
+                <div className="text-center mt-2 px-4">
+                  One of the top 100 advertising and marketing agencies knows
+                  what it means to be
+                </div>
+                <div className="readMorebutton mt-2 mb-4 text-center">
+                  <a href="#top">
+                    <b>Read More</b>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
     </>
   );
 }
